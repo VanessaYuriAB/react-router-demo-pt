af09208921979e3c268d89787ae82673602288d4
VanessaYuriAB <emaildavanessayuri@gmail.com>
2025-07-07 04:43:15 -0300

    feat: create AboutMe component with navigation links and content sections

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,7 +1,22 @@
+import './AboutMe.css';
+import { Outlet, Link } from 'react-router-dom';
+
 function AboutMe() {
   return (
-    <div>
+    <div className="about">
+      <ul className="links">
+        <li>
+          <Link to="my-story">Minha História</Link>
+        </li>
+        <li>
+          <Link to="hobbies">Hobbies</Link>
+        </li>
+        <li>
+          <Link to="contact">Informações de Contato</Link>
+        </li>
+      </ul>
       <p>Eu sou uma pessoa simples. Eu vejo Emojis, eu escrevo avaliações.</p>
+      <Outlet />
     </div>
   );
 }
