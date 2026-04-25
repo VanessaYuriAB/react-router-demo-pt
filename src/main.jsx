ccfa617dd430fb47bb6ff4a85512425c28b2b9da
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-04-25 17:19:22 -0300

    refactor(router): apply HashRouter for compatibility with GitHub Pages
    Replace BrowserRouter with HashRouter for compatibility with GitHub Pages deployment. It also avoid 404 errors on GitHub Pages routes.

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import App from './components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* Aninhamento do componente App em um BrowserRouter para que componentes Route funcionem corretamente e possamos criar rotas no nosso aplicativo(página) */}
-    <BrowserRouter>
+    {/* Aninhamento do componente App em um BrowserRouter para que componentes Route 
+    funcionem corretamente e possamos criar rotas no nosso aplicativo (página) */}
+
+    {/* BrowserRouter refatorado para HashRouter para deploy no GitHub Pages */}
+
+    <HashRouter>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    </HashRouter>
+  </React.StrictMode>,
 );
